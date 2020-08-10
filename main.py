241124763719e6fd4bb9ee6309027e584ecc6d6d
Rishi Shah <36748900+RishiShah200@users.noreply.github.com>
2020-08-10 17:03:28 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/calculators")
+def calculators():
+  return render_template("calculators.html")
+
+@app.route("/loans")
+def loans():
+  return render_template("loans.html")
+
+@app.route("/testimonials")
+def testimonials():
+  return render_template("testimonials.html")
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
+@app.route("/calculators/mortgage_calculators")
+def mortgage_calculators():
+  return render_template("calculators/mortgage_calculators.html")
+
+@app.route("/calculators/principal_calculator")
+def principal_calculator():
+  return render_template("calculators/principal_calculator.html")
+
+@app.route("/calculators/affordability_calculator")
+def affordability_calculator():
+  return render_template("calculators/affordability_calculator.html")
+
+@app.route("/report_issue")
+def report_issue():
+  return render_template("report_issue.html")
+
+
+@app.route("/loan_programs")
+def loan_programs():
+  return render_template("loan_programs.html")
+
+# @app.route('/report_issue', methods=['GET', 'POST'])
+# def form():
+#     return render_template('report_isue.html')
+
+# @app.route('/hello', methods=['GET', 'POST'])
+# def hello():
+#     return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/template.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
+
+// document.getElementById("p1").innerHTML = "New text!";
+
+const email = document.getElementById("email");
+const email_help = document.getElementById("email_help");
+
+
+email.addEventListener("input", function (event) {
+    if(email.validity.valid === false){
+        email.classList.add("is-danger")
+        email_help.classList.remove("is-success")
+        email_help.classList.add("is-danger")
+        email_help.innerHTML = "This email is invalid"
+    }
+    else{
+        email.classList.remove("is-danger")
+        email.classList.add("is-success")
+        email_help.classList.remove("is-danger")
+        email_help.classList.add("is-success")
+        email_help.innerHTML = "This email is valid"
+        
+    }
+    
+    })
+
+const user_name = document.getElementById("user_name");
+const name_help = document.getElementById("name_help");
+
+user_name.addEventListener("input", function (event) {
+    if(user_name.validity.valid === false){
+        user_name.classList.add("is-danger")
+    }
+    else{
+        user_name.classList.remove("is-danger")
+        user_name.classList.add("is-success")
+        name_help.classList.remove("is-danger")
+        name_help.classList.add("is-success")
+    }
+    
+    })
+
+const mode_toggle = document.getElementById("switch_example");
+
+mode_toggle.addEventListener("input", function (event) {
+    if (mode_toggle.checked.checked === false){
+        console.log("cheecking");
+    }
+
+    })
